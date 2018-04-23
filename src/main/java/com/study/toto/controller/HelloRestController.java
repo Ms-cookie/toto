9cbe519a1bd890d26e27d1990b9df7e22e6bf348
09233 <09233@SKCC16N00025.SKCC.NET>
2018-04-23 17:50:14 +0900

    add SrpingBoot Configurations

--- src/main/java/com/study/toto/controller/HelloRestController.java
+++ src/main/java/com/study/toto/controller/HelloRestController.java
@@ -1,5 +1,13 @@
 package com.study.toto.controller;
 
+import java.util.HashMap;
+
+@RestController
 public class HelloRestController {
-    // TODO 2 "/"로 요청이 들어오면 "Hello"을 리턴하는 RestController 작성
+	// TODO 2 "/"로 요청이 들어오면 "Hello"을 리턴하는 RestController 작성
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "Hello";
+	}
 }
